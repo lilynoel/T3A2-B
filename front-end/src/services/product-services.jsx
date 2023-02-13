73e3b3f87edd83b720c0490253d9186f008c1033
lilynoel <lilydnas@gmail.com>
2023-02-14 01:28:22 +1100

    back-end

--- front-end/src/services/product-services.jsx
+++ front-end/src/services/product-services.jsx
@@ -1,6 +1,8 @@
 import products from './fake-data.json';
 
 export async function getProducts() {
+    const response = await fetch('http://localhost:3000/products');
+    const products = await response.json();
     return products;
 }
 
