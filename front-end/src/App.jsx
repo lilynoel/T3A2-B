b6b33c209a009db931491346cc205fc9bde1430f
lilynoel <lilydnas@gmail.com>
2023-02-14 00:03:29 +1100

    nav bar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,18 +7,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/HomePage';
 import ProductPageLoader from './containers/ProductPageLoader/ProductPageLoader';
 import NotFound from './components/NotFound/NotFound';
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <h1>Big Heading</h1>
+        <NavBar/>
+
         <Routes> 
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<ProductList />} />
           <Route path='/products/:id' element={<ProductPageLoader />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
+
+        <footer>My Paws</footer>
       </BrowserRouter>
     </div>
   );
