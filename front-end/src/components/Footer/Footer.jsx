dbd712d14fa250c348b9bf4286116a30b3dde015
lilynoel <lilydnas@gmail.com>
2023-02-15 03:06:37 +1100

    footer update

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
       <footer className={styles.footer}>
-          Example Footer
+          Contact us | +614 567 123 |
+          Bone & Biscuit. © 2023 PJ & Lily Noël.
     </footer>
   )
 }
