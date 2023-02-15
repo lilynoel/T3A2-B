c5e8c0ff2825138f036490b1eaeef9c041809a41
lilynoel <lilydnas@gmail.com>
2023-02-15 16:14:43 +1100

    product page update

--- front-end/src/pages/Product/ProductPage.jsx
+++ front-end/src/pages/Product/ProductPage.jsx
@@ -1,26 +1,44 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import { getProductById } from '../../services/product-services';
 import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
 import { UserContext } from '../../context/UserContextProvider';
 
-
 export default function ProductPage(props) {
-    const { product } = props;
-    const { userDetails } = useContext(UserContext);
-    const { username } = userDetails;
+  const [quantity, setQuantity] = useState(1);
+  const { product } = props;
+  const { userDetails } = useContext(UserContext);
+  const { username } = userDetails;
 
-    return (
+  const onDecrementButtonClick = () => {
+    if (quantity < 2) {
+      setQuantity(1);
+    } else {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const onIncrementButtonClick = () => {
+    setQuantity(quantity + 1);
+  };
+
+  return (
     <>
-        {
+      {
         <div>
-            <h1>{product.name}</h1>
-            <h2>${product.price}</h2>
-                    <img src={product.url} alt={product.name} />
-                    {username && <button>Create Order</button>}
-         
+          <h1>{product.name}</h1>
+          <h2>${product.price}</h2>
+          <img src={product.url} alt={product.name} />
+          {username && (
+            <>
+              <p>Quantity: {quantity}</p>
+              <button onClick={onDecrementButtonClick}>-</button>
+              <button onClick={onIncrementButtonClick}>+</button>
+              <button>Create Order</button>
+            </>
+          )}
         </div>
-        }
+      }
     </>
-    );
+  );
 }
-
